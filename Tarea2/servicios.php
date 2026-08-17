66745e392776ad0f6bfb5f0dc6276b08f2ff1783
Reynner <reynnerontiveros@gmail.com>
2026-08-16 23:18:39 -0400

    Commit rama secundaria

--- Tarea2/servicios.php
+++ Tarea2/servicios.php
@@ -1,9 +1,57 @@
-<html>
+<!DOCTYPE html>
+<html lang="es">
     <head>
-        <title>Primera servicios</title>
+        <title>Primera pagina</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>        
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     </head>
     <body>
-        Hola servicios
-        <a href ="index.php">Volver</a>
+        <!-- Navbar -->
+        <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+            <div class="container-fluid"> 
+                <a class="navbar-brand" href="index.php"><i class="fa fa-car"></i></a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="collapsibleNavbar">
+                    <ul class="navbar-nav">
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="empresa.php" role="button" data-bs-toggle="dropdown">Empresa</a>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="#">Quienes Somos</a></li>
+                                <li><a class="dropdown-item" href="#">Nuestro Equipo</a></li>
+                                <li><a class="dropdown-item" href="#">Mision</a></li>
+                            </ul>
+                        </li>                        
+                        <li class="nav-item">
+                            <a class="nav-link" href="productos.php">Productos</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="servicios.php">Servicios</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="contacto.php">Contacto</a>
+                        </li>  
+                    </ul>
+                </div>                
+            </div>
+        </nav>       
+        <!-- Container -->
+        <div class="container-fluid bg-warning">
+            Hola Servicios<br>
+            <a href="index.php">Volver</a>
+        </div>
+        <!-- Footer -->   
+        <div class="container-fluid bg-dark">
+            <div class="row">
+                <div class="col-4"></div>
+                <div class="col-4" style="color:white"><strong>MiEmpresa@2026</strong></div>
+                <div class="col-4"></div>
+            </div>
+        </div>
+        <!-- Modal -->     
     </body>
-</html> 
+</html>
